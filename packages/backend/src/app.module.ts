d5097e7096f97e66077a68447d358a14b12c15f3
PSW99 <steamdayz@naver.com>
2026-01-06 21:47:55 +0900

    feat: AppModule 수정 및 로깅 활성화
    - AppModule에 FeedbackModule 및 WinstonModule 추가.
    - nest-winston 기반의 feedbackLoggerConfig 설정 활성화.

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,16 +5,24 @@ import { MatchModule } from './match/match.module';
 import { QuizModule } from './quiz/quiz.module';
 import { HealthController } from './health/health.controller';
 import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import { feedbackLoggerConfig } from './common/winston.config';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    WinstonModule.forRoot(feedbackLoggerConfig),
+
     QuizModule,
     MatchModule,
+    FeedbackModule,
   ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
-export class AppModule {}
+class AppModule {}
+
+export default AppModule;
