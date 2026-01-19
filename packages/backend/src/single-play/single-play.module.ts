22a0b24267c7f99faa8388e7d3ed6ccfd5714e36
PSW99 <steamdayz@naver.com>
2026-01-19 20:25:15 +0900

    feat: SinglePlayService 모듈 등록
    - SinglePlayModule에 SinglePlayService 추가.
    - 모듈 내 서비스 의존성으로 SinglePlayService 등록.

--- packages/backend/src/single-play/single-play.module.ts
+++ packages/backend/src/single-play/single-play.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SinglePlayController } from './single-play.controller';
+import { SinglePlayService } from './single-play.service';
 import { Category, Question } from '../quiz/entity';
 import { QuizService } from '../quiz/quiz.service';
 import { ClovaClientService } from '../quiz/clova/clova-client.service';
@@ -8,6 +9,6 @@ import { ClovaClientService } from '../quiz/clova/clova-client.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Category, Question])],
   controllers: [SinglePlayController],
-  providers: [QuizService, ClovaClientService],
+  providers: [SinglePlayService, QuizService, ClovaClientService],
 })
 export class SinglePlayModule {}
